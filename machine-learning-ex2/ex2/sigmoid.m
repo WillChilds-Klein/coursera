80ffa2143b470c8387090da6fdd41e84cc19742a
WillChilds-Klein <willck93@gmail.com>
2016-08-18 01:04:52 -0400

    1.2.1) implement sigmoid funciton for scalars, vectors, and matrices

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -8,11 +8,19 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
+if (isvector(z) || ismatrix(z))
+    for i = 1:rows(z)
+        for j = 1:columns(z)
+            g(i,j) = single_sigmoid(z(i,j));
+        end
+    end
+elseif (isscalar(z))
+    g = single_sigmoid(z);
+end
 % =============================================================
 
+end
+
+function g = single_sigmoid(z)
+    g = 1 / (1 + e^(-1*z));
 end
